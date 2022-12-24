7cb68fa91436cd53c5739b88fd08915945130757
Devin Howard <devin@callysto.com>
2022-12-23 19:09:01 -0500

    Wire up HP and initiative calculations

--- src/logic.js
+++ src/logic.js
@@ -3,9 +3,10 @@ export function getStats(answers) {
   const orderedList = entriesSortedByValue(statPreferences);
   const standardArray = [15, 14, 13, 12, 10, 8];
   const stats = Object.fromEntries(orderedList.map((element, index) => {
-    const key = element[0];
+    const statName = element[0];
     const value = standardArray[index];
-    return [key, value];
+    const modifier = getModifier(value);
+    return [statName, {value, modifier}];
   }));
   return stats;
 }
@@ -13,18 +14,15 @@ export function getStats(answers) {
 export function getClass(answers) {
   const classPreferences = (({ war, wiz, cle, rog }) => ({ war, wiz, cle, rog }))(answers);
   const orderedList = entriesSortedByValue(classPreferences);
-  const shortName = orderedList[0][0];
-  const nameMappings = {
-    war: 'Fighter',
-    wiz: 'Wizard',
-    cle: 'Cleric',
-    rog: 'Rogue'
-  };
-  return nameMappings[shortName];
+  return orderedList[0][0];
 }
 
 // e.g. turns { a: 1, b: 2, c: 0 }
 // into [['b', 2], ['a', 1], ['c', 0]]
 function entriesSortedByValue(obj) {
   return Object.entries(obj).slice().sort((a, b) => b[1] - a[1]);
+}
+
+function getModifier(statValue) {
+  return Math.floor((statValue - 10) / 2);
 }
--- src/render.js
+++ src/render.js
@@ -1,26 +1,52 @@
 import outdent from 'outdent';
 
 export function render(stats, klass) {
+  const className = renderClassName(klass);
   const { str, dex, con, int, wis, cha } = renderStats(stats);
+  const initiative = renderModifier(stats.dex.modifier);
+  const health = getHealth(klass, stats.con.modifier);
 
   return outdent`
     ${outdent}
-            <Your Name>, Level 1 Human ${klass}
+            <Your Name>, Level 1 Human ${className}
     Str: ${str}        Proficiency: +2
-    Dex: ${dex}        Initiative: +2
-    Con: ${con}        Health: 6 / 6
+    Dex: ${dex}        Initiative: ${initiative}
+    Con: ${con}        Health: ${health} / ${health}
     Int: ${int}        
     Wis: ${wis}
     Cha: ${cha}
   `;
 }
 
+function getHealth(klass, conMod) {
+  const healthMappings = {
+    war: 10,
+    cle: 8,
+    rog: 8,
+    wiz: 6
+  };
+  return healthMappings[klass] + conMod;
+}
+
+function renderModifier(modifier) {
+  const symbol = modifier < 0 ? '-' : '+';
+  return `${symbol}${Math.abs(modifier)}`;
+}
+
 function renderStats(stats) {
-  return Object.fromEntries(Object.entries(stats).map(([statName, statValue]) => {
-    const modifier = Math.floor((statValue-10)/2);
-    const statNumber = `${statValue}`.padStart(2, ' ');
-    const symbol = modifier < 0 ? '-' : '+';
-    const statString = `${statNumber} (${symbol}${Math.abs(modifier)})`;
+  return Object.fromEntries(Object.entries(stats).map(([statName, { value, modifier }]) => {
+    const statNumber = `${value}`.padStart(2, ' ');
+    const statString = `${statNumber} (${renderModifier(modifier)})`;
     return [statName, statString];
   }));
+}
+
+function renderClassName(klass) {
+  const nameMappings = {
+    war: 'Fighter',
+    wiz: 'Wizard',
+    cle: 'Cleric',
+    rog: 'Rogue'
+  };
+  return nameMappings[klass];
 }
